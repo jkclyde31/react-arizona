a46563f1716d74398dbe379d86a2ef30cf396403
Clyde <randa.technodream@gmail.com>
2024-08-26 16:00:34 +0800

    updated 'gallery'

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import ImageGallery from "../components/imageGallery";
-import images from "../utils/loadImages";
+// import React from "react";
+// import ImageGallery from "../components/imageGallery";
+// import images from "../utils/loadImages";
 
+
+// const Gallery = () => {
+//   return (
+//     <div id="content">
+//      <div className="row">
+//       <h1 className="text-center">Gallery</h1>
+//       <ImageGallery images={images} imagesPerPage={10} />
+//      </div>
+//     </div>
+//   );
+// };
+
+// export default Gallery;
+
+import React from 'react'
 
 const Gallery = () => {
   return (
-    <div id="content">
-     <div className="row">
-      <h1 className="text-center">Gallery</h1>
-      <ImageGallery images={images} imagesPerPage={10} />
-     </div>
-    </div>
-  );
-};
+    <div>Gallery</div>
+  )
+}
 
-export default Gallery;
+export default Gallery
